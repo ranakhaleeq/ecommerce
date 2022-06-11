63ba1857a106ae353484a11d9689f9790a5a37f5
ranakhaleeqrk <ranakhaleeqrk@gmail.com>
2022-06-11 07:21:22 +0500

    run by pom

--- src/main/java/com/ecommerce/testCases/BaseClass.java
+++ src/main/java/com/ecommerce/testCases/BaseClass.java
@@ -1,29 +1,30 @@
 package com.ecommerce.testCases;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import com.ecommerce.utilities.ReadConfig;
+//import com.ecommerce.utilities.ReadConfig;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	 
 	WebDriver driver; 
-	ReadConfig readConfig = new ReadConfig();
-
-	public String baseUrl = readConfig.getBaseURL();
+//	ReadConfig readConfig = new ReadConfig();
+//
+//	public String baseUrl = readConfig.getBaseURL();
 	
 	@Test
-	public void start() {
+	public void start() { 
 		
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
-		driver.get(baseUrl);
+		driver.get("http://automationpractice.com/index.php");
 	}
 	
-	
+	 
 }
