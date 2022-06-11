d813239cf9df925e93b48aeb73116431c3f18210
ranakhaleeqrk <ranakhaleeqrk@gmail.com>
2022-06-11 06:19:09 +0500

    first test case

--- src/main/java/com/ecommerce/testCases/BaseClass.java
+++ src/main/java/com/ecommerce/testCases/BaseClass.java
@@ -1,9 +1,12 @@
 package com.ecommerce.testCases;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import com.ecommerce.utilities.ReadConfig;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	 
@@ -15,5 +18,12 @@ public class BaseClass {
 	@Test
 	public void start() {
 		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
 	}
+	
+	
 }
